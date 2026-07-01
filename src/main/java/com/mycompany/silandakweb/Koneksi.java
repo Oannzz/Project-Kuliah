07ac9bafc8954b02def25a29db99c577da9b6979
Fadhli-cahyo <fadlicahyo02@gmail.com>
2026-07-02 00:03:35 +0700

    update database

--- src/main/java/com/mycompany/silandakweb/Koneksi.java
+++ src/main/java/com/mycompany/silandakweb/Koneksi.java
@@ -9,7 +9,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Koneksi {
-    private static final String URL = "jdbc:mysql://localhost:3306/silandak1?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/silandak?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root"; // Sesuaikan dengan username MySQL Anda
     private static final String PASSWORD = ""; // Sesuaikan dengan password MySQL Anda
 
